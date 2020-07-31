d7f37840ce16ce1a30bf898bb785d8edbbab84a7
novin997 <cocolupiayi@gmail.com>
2020-07-31 22:37:05 +0800

    fix navbar

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,53 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <h1>Novin's Anime Webpage</h1>
-      <div class="navlink">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/animelist">AnimeList</router-link>
-      </div>
-    </div>
+    <b-navbar class="navbar" variant="dark" type="light">
+        <b-link class="m-2 text-white" to ="/">
+          Home
+        </b-link>
+        <b-link class="m-2 text-white" to ="/animelist">
+          AnimeList
+        </b-link>
+    </b-navbar>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import 'node_modules/bootstrap/scss/bootstrap';
+  @import 'node_modules/bootstrap-vue/src/index.scss';
 
-#nav {
-  background-color: lime;
-  padding: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  
-  & > * {
-    margin:10px;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
   }
 
-  h1 {
-    color:black;
-    margin-right:auto;
-    margin-left:200px;
+  .navbar a:hover{
+    text-decoration:none;
   }
 
-  .navlink{
-    margin:auto;
-
-    a {
-      font-weight: bold;
-      color: white;
-
-      &.router-link-exact-active {
-        color: dimgray;
-      }
-    }
-  }
-}
 </style>
